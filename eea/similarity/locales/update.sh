02e588ec7221f9e72f9773214aa21465b1943e0b
Alin Voinea <alin@eaudeweb.ro>
2017-04-14 11:18:05 +0300

    i18n

--- eea/similarity/locales/update.sh
+++ eea/similarity/locales/update.sh
@@ -6,7 +6,7 @@
 # locales/[language]/LC_MESSAGES/eea.similarity.po
 # (e.g. locales/de/LC_MESSAGES/eea.similarity.po)
 
-domain=eea.similarity
+domain=eea
 
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
